cdd05d5a39fe3d9401f7b34fe19ea00e8ce7ef7e
mlongi11 <mlongi@chicagobooth.edu>
2022-04-14 00:26:28 +0000

    updated models and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,16 +23,16 @@ class User < ApplicationRecord
 
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
   has_many :comments, foreign_key: "author_id"
-  has_many :received_requests, class_name: "FollowRequest", foreign_key: "recipient_id"
-  has_many :sent_requests, class_name: "FollowRequest", foreign_key: "sender_id"
+  has_many :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id"
+  has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id"
   has_many :likes, foreign_key: "fan_id"
 
   has_many :accepted_received_follow_request, -> {accepted}, class_name: "FollowRequest", foreign_key: "recipient_id"
   has_many :accepted_sent_follow_request, -> {accepted}, class_name: "FollowRequest", foreign_key: "sender_id"
-  has_many :leaders, through: :accepted_sent_requests, source: :recipient
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
   has_many :discover, through: :leaders, source: :liked_photos
   has_many :feed, through: :leaders, source: :own_photos
-  has_many :followers, through: :accepted_received_requests, source: :sender
+  has_many :followers, through: :accepted_received_received_requests, source: :sender
   has_many :like_photos, through: :likes, source: :photo
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
