eaa1c943e0b823d8441f0d2627d084877d499a7a
mlongi11 <mlongi@chicagobooth.edu>
2022-04-20 20:14:23 +0000

    done with liked

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
 
-  get "/:username" => "users#show"
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "photos#liked", as: :liked_photos
+  get ":username/followers" => "photos#liked", as: :liked_photos
+  get ":username/following" => "photos#liked", as: :liked_photos
+
+  get ":username" => "users#show", as: :user
 end
