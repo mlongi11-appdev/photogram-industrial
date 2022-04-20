bfc718efb16842c5a97daa6d6c3a2cf36fff75a3
mlongi11 <mlongi@chicagobooth.edu>
2022-04-20 20:40:23 +0000

    done with leaders, followers, feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
     @user = User.find_by!(username: params.fetch(:username))
   end
 
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def followers
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def leaders
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   resources :photos
 
   get ":username/liked" => "photos#liked", as: :liked_photos
-  get ":username/feed" => "photos#liked", as: :liked_photos
-  get ":username/followers" => "photos#liked", as: :liked_photos
-  get ":username/following" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#leaders", as: :leaders
 
   get ":username" => "users#show", as: :user
 end
